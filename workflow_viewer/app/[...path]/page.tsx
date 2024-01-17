65b26cc99d81e3efdb7cc2b676b7a9d868f4bd1c
Kyle Liebler <liebler@umich.edu>
2024-01-17 06:15:17 -0500

    add sortable and searchable table component to use as file/directory explorer

--- workflow_viewer/app/[...path]/page.tsx
+++ workflow_viewer/app/[...path]/page.tsx
@@ -1,5 +1,13 @@
+import fetchData from "../../lib/fetchData";
+import { TableSort } from "../../components/TableSort/TableSort";
 import Shell from "../../components/Shell";
 
-export default function HomePage() {
-  return <Shell><></></Shell>;
+export default async function PathPage({ params }: any) {
+  const data = await fetchData(params.path ?? []);
+
+  return (
+    <Shell>
+      <TableSort data={data} />
+    </Shell>
+  );
 }
---
+++ workflow_viewer/components/TableSort/TableSort.tsx
@@ -0,0 +1,177 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+  rem,
+  Flex,
+  Anchor,
+  useMantineTheme,
+} from "@mantine/core";
+import {
+  IconSelector,
+  IconChevronDown,
+  IconChevronUp,
+  IconSearch,
+  IconFolder,
+  IconFileText,
+} from "@tabler/icons-react";
+import classes from "./TableSort.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { RowData } from "../../lib/directoryStructure";
+
+interface ThProps {
+  children: React.ReactNode;
+  reversed: boolean;
+  sorted: boolean;
+  onSort(): void;
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+  const Icon = sorted
+    ? reversed
+      ? IconChevronUp
+      : IconChevronDown
+    : IconSelector;
+  return (
+    <Table.Th className={classes.th}>
+      <UnstyledButton onClick={onSort} className={classes.control}>
+        <Group justify="space-between">
+          <Text fw={500} fz="sm">
+            {children}
+          </Text>
+          <Center className={classes.icon}>
+            <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+          </Center>
+        </Group>
+      </UnstyledButton>
+    </Table.Th>
+  );
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim();
+  return data.filter((item) => item.name.toLowerCase().includes(query));
+}
+
+function sortData(
+  data: RowData[],
+  payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+  const { sortBy } = payload;
+
+  if (!sortBy) {
+    return filterData(data, payload.search);
+  }
+
+  return filterData(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return b[sortBy].localeCompare(a[sortBy]);
+      }
+
+      return a[sortBy].localeCompare(b[sortBy]);
+    }),
+    payload.search
+  );
+}
+
+interface TableSortProps {
+  data: RowData[];
+}
+
+export function TableSort({ data }: TableSortProps) {
+  const theme = useMantineTheme();
+  const path = ((p) => (p.endsWith("/") ? p.slice(0, -1) : p))(usePathname());
+  const [search, setSearch] = useState("");
+  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+  const setSorting = (field: keyof RowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(sortData(data, { sortBy: field, reversed, search }));
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearch(value);
+    setSortedData(
+      sortData(data, { sortBy, reversed: reverseSortDirection, search: value })
+    );
+  };
+
+  const rows: any = sortedData.map((row: any) => (
+    <Table.Tr key={row.name}>
+      <Table.Td>
+        <Flex justify="space-between">
+          {row.file ? (
+            row.name
+          ) : (
+            <Anchor
+              component={Link}
+              href={`${path}/${row.uuid}`}
+              underline="never"
+              c={theme.colors.dark[0]}
+            >
+              {row.name}
+            </Anchor>
+          )}
+          {row.file ? <IconFileText /> : <IconFolder />}
+        </Flex>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <ScrollArea>
+      <TextInput
+        placeholder="Search by any field"
+        mb="md"
+        leftSection={
+          <IconSearch
+            style={{ width: rem(16), height: rem(16) }}
+            stroke={1.5}
+          />
+        }
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <Table horizontalSpacing="md" verticalSpacing="xs" layout="fixed">
+        <Table.Tbody>
+          <Table.Tr>
+            <Th
+              sorted={sortBy === "name"}
+              reversed={reverseSortDirection}
+              onSort={() => setSorting("name")}
+            >
+              Name
+            </Th>
+          </Table.Tr>
+        </Table.Tbody>
+        <Table.Tbody>
+          {rows.length > 0 ? (
+            rows
+          ) : (
+            <Table.Tr>
+              <Table.Td>
+                <Text fw={500} ta="center">
+                  Nothing found
+                </Text>
+              </Table.Td>
+            </Table.Tr>
+          )}
+        </Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
